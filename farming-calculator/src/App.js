fe6da15d676d643ddf84da07470782abc25b5850
Daniel Chapman <danielc@my.ccsu.edu>
2020-05-14 16:41:24 -0400

    Day to day function basically done

--- farming-calculator/src/App.js
+++ farming-calculator/src/App.js
@@ -7,6 +7,11 @@ import {generateLevel } from './function';
 import TimeCalculations from './components/TimeCalculation';
 import ExperienceView from './components/ExperienceView';
 
+import {fruittrees} from './data/FruitTrees';
+import {trees} from './data/Trees';
+
+
+//Dont forget farming outfit
 class App extends Component {
 
   constructor(props) {
@@ -21,6 +26,56 @@ class App extends Component {
       goalExperience: 13034431,
       currentLevel: 1,
       goalLevel: 99,
+
+      planting: {
+        trees: {
+          patches: {
+            lumbridge: {
+              numberPlanted: 0,
+              maxNumberPlanted: 2,
+              type: 'magic',
+              growth: trees['magic'].growth,
+              planted: false
+
+            },
+            varrock: {
+              numberPlanted: 0,
+              maxNumberPlanted: 2,
+              type: 'magic',
+              growth: trees['magic'].growth,
+              planted: false
+            },
+            falador: {
+              numberPlanted: 0,
+              maxNumberPlanted: 2,
+              type: 'magic',
+              growth: trees['magic'].growth,
+              planted: false
+            },
+            taverly: {
+              numberPlanted: 0,
+              maxNumberPlanted: 2,
+              type: 'magic',
+              growth: trees['magic'].growth,
+              planted: false,
+            },
+            stronghold: {
+              numberPlanted: 0,
+              maxNumberPlanted: 2,
+              type: 'magic',
+              growth: trees['magic'].growth,
+              planted: false,
+            },
+            guild: {
+              numberPlanted: 0,
+              maxNumberPlanted: 2,
+              type: 'magic',
+              growth: trees['magic'].growth,
+              planted: false,
+            },
+          }
+        }
+      }
     }
   }
 
@@ -28,6 +83,7 @@ class App extends Component {
 
     let state = this.state;
 
+    let newDay = false;
 
     if (state.startCounting) {
       state.minutes += 1;
@@ -38,17 +94,42 @@ class App extends Component {
       if (state.hours === 24) {
           state.hours = 0;
           state.days++;
+          newDay = true;
       }
   
       //Calculations for each one
-  
-      if (state.days < 2) {
+      //trees
+      let x = state.planting.trees.patches;
+      Object.keys(x).map((i) => {
+        if (newDay) {
+          x[i].numberPlanted = 0;
+        }
+        if (x[i].numberPlanted > x[i].maxNumberPlanted) {
+          console.log('here');
+          return null;
+        }
+        if (!x[i].planted && x[i].numberPlanted <= x[i].maxNumberPlanted) {
+          x[i].planted = true;
+          x[i].numberPlanted += 1;
+          state.currentExperience += trees[x[i].type].planting;
+          console.log('planted');
+        }
+        
+        x[i].growth -= 1;
+        if (x[i].growth === 0) {
+          state.currentExperience += trees[x[i].type].checking;
+          state.currentExperience += trees[x[i].type].harvest;
+          x[i].planted = false;
+          x[i].growth =  trees[x[i].type].growth;
+          console.log(`magic tree done at location ${i}`);
+        }
+      })
+      if (state.days < 10) {
           setTimeout(() => {
               this.setState({...state});
           }, 1);
-          
       } 
-      if (state.days >= 2 && !state.finalUpdate ) {
+      if ((state.days >= 10 && !state.finalUpdate) || (state.currentExperience >= state.goalExperience) ) {
           state.finalUpdate = true;
           state.startCounting = false;
           console.log(generateLevel(90, false));
---
+++ farming-calculator/src/data/Trees.js
@@ -0,0 +1,44 @@
+export const trees = {
+    oak: {
+        name: 'Oak',
+        level: 15,
+        planting: 14,
+        checking: 467.2,
+        harvest: 37.5,
+        growth: 140,
+    },
+    willow: {
+        name: 'Willow',
+        level: 30,
+        planting: 25,
+        checking: 1456.3,
+        harvest: 67.5,
+        growth: 230,
+    },
+    maple: {
+        name: 'Maple',
+        level: 45,
+        planting: 45,
+        checking: 3403.4,
+        harvest: 100,
+        growth: 320,
+    },
+    yew: {
+        name: 'Yew',
+        level: 60,
+        planting: 81,
+        checking: 7069.9,
+        harvest: 175,
+        growth: 400,
+    },
+    magic: {
+        name: 'Magic',
+        level: 75,
+        planting: 145.5,
+        checking: 13768.3,
+        harvest: 250,
+        growth: 480,
+    },
+};
+
+export const patches = ['Falador Park', 'Lumbridge', 'Taverly', 'Gnome Stronghold', 'Farming Guild', 'Varrock'];
